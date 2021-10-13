5d57e7a36b443d124a8ec01c4b41dfaa3f41bb15
vitran96 <tranuytrieuvi@gmail.com>
2021-10-13 07:51:37 +0700

    update

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -15,9 +15,23 @@ connect.then((db) => {
     }).then((dish) => {
         console.log(dish);
 
-        return Dishes.find({});
-    }).then((dishes) => {
-        console.log(dishes);
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { scription: 'Updated test' }
+        }, {
+            new: true
+        }).exec();
+    }).then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: "I'm getting a sinking feeling!",
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    }).then((dish) => {
+        console.log(dish);
 
         return Dishes.remove({});
     }).then(() => {
