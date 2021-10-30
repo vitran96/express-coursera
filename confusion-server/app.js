93f23a1ce8c9cdaf1d2728aabff12b0162a8ebc6
vitran96 <tranuytrieuvi@gmail.com>
2021-10-31 06:39:30 +0700

    user session, login, signup, logout

--- confusion-server/app.js
+++ confusion-server/app.js
@@ -32,7 +32,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser(SECRET));
 app.use(session({
   name: 'session-id'
   , secret: SECRET
@@ -41,12 +40,10 @@ app.use(session({
   , store: new FileStore()
 }))
 
-function auth(req, res, next) {
-  // console.log(req.headers);
-  // console.log(req.signedCookies);
+const auth = (req, res, next) => {
   console.log(req.session);
   if (req.session.user) {
-    if (req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') {
       next();
     } else {
       var err = new Error('You are not authenticated!');
@@ -57,47 +54,27 @@ function auth(req, res, next) {
     return;
   }
 
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
-    return;
-  }
-
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
-  if (user == 'admin' && pass == 'password') {
-    // res.cookie('user', 'admin', { signed: true });
-    req.session.user = 'admin';
-    next(); // authorized
-  } else {
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
-  }
+  var err = new Error('You are not authenticated!');
+  res.setHeader('WWW-Authenticate', 'Basic');
+  err.status = 401;
+  next(err);
 }
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
-app.use('/leaders', leaderRouter);
+app.use('/dishes', auth, dishRouter);
+app.use('/promotions', auth, promoRouter);
+app.use('/leaders', auth, leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
