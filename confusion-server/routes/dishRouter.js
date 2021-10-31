62e593091bc325c20d106864c6c5ad717e9d685b
vitran96 <tranuytrieuvi@gmail.com>
2021-10-31 11:23:41 +0700

    remove comment

--- confusion-server/routes/dishRouter.js
+++ confusion-server/routes/dishRouter.js
@@ -228,9 +228,6 @@ dishRouter.route('/:dishId/comments/:commentId')
     .put(
         authenticate.verifyUser
         , (req, res, next) => {
-            // TODO: Allow a registered user to submit comments (already completed), update a submitted comment and delete a submitted comment.
-            // The user should be restricted to perform such operations only on his/her own comments.
-            // No user or even the Admin can edit or delete the comments submitted by other users
             Dishes.findById(req.params.dishId)
                 .then((dish) => {
                     if (dish
