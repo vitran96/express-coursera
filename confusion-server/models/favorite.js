7cd3a75099bbd572c7482588df0b09ea888b7093
--global <--global>
2021-11-10 21:17:14 +0700

    refactor

--- confusion-server/models/favorite.js
+++ confusion-server/models/favorite.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const favoriteSchema = new Schema({
-    user_id: {
+    user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
-    favoriteDishes: [{
+    dishes: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Dish'
     }]
