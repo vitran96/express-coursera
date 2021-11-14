601d446c82f36e1611c063d3ca7841401f549dd9
Tran Uy Trieu Vi <tranuytrieuvi@gmail.com>
2021-11-14 10:40:21 +0700

    add React Loopback implementation

--- conFusionServer-Loopback/server/boot/root.js
+++ conFusionServer-Loopback/server/boot/root.js
@@ -7,7 +7,8 @@
 
 module.exports = function(server) {
   // Install a `/` route that returns server status
-  const router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
+
+  // const router = server.loopback.Router();
+  // router.get('/', server.loopback.status());
+  // server.use(router);
 };
